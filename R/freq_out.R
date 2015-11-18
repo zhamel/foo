eaf9e02120b6f7cac5a8a020071707bb2ba6d216
zhamel <zhamel92@gmail.com>
2015-11-17 17:45:18 -0800

    freq_out update

--- R/freq_out.R
+++ R/freq_out.R
@@ -1,13 +1,12 @@
-#' Create a frequency table for a factor
+#' Make a frequency table for a factor
 #'
 #' @param x factor
 #'
 #' @return tbl_df
 #' @export
-#'
 #' @examples
 #' freq_out(iris$Species)
-freq_out <- function(x){
+freq_out <- function(x) {
   xdf <- dplyr::data_frame(x)
-  dplyr::count(xdf,x)
+  dplyr::count(xdf, x)
 }
