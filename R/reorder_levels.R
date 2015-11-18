5aee346bbf4c3e16e99f87342aa2e84d0be47761
zhamel <zhamel92@gmail.com>
2015-11-18 07:57:14 -0800

    update dplyr >%>

--- R/reorder_levels.R
+++ R/reorder_levels.R
@@ -4,10 +4,12 @@
 #'
 #' @return factor
 #' @export
+#' @importFrom dplyr %>%
 #' @examples
+#' reorder_levels(factor(c("a","a","b","c","c","c","c","d","d","d")))
 reorder_levels <- function(x){
   assertthat::assert_that(is.factor(x))
   xdf <- dplyr::data_frame(x)
-  count <- xdf %>% dplyr::group_by(x) %>% tally()
-  factor(reorder(count$x,count$n,dplyr::desc))
+  count <- xdf %>% dplyr::group_by(x) %>% dplyr::tally()
+  factor(stats::reorder(count$x,count$n,dplyr::desc))
 }
