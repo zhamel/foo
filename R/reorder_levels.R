1c445c16cc2c18b0867b8a4815f23eeaf32070fa
zhamel <zhamel92@gmail.com>
2015-11-18 01:06:25 -0800

    more

--- R/reorder_levels.R
+++ R/reorder_levels.R
@@ -6,6 +6,7 @@
 #' @export
 #' @examples
 reorder_levels <- function(x){
+  assertthat::assert_that(is.factor(x))
   xdf <- dplyr::data_frame(x)
   count <- xdf %>% dplyr::group_by(x) %>% tally()
   factor(reorder(count$x,count$n,dplyr::desc))
